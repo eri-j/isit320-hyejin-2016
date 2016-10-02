0f6b5b5758b08770aa734ec3570dfe0c3f55433a
Hyejin Jun <hyejin.jun@bellevuecollge.edu>
2016-10-01 22:39:42 -0700

    GetNumbers & ObjectBasicsJasmine for JasmineServerBasic

---
+++ Week02-ObjectBasicsJasmine/Source/MyObject.js
@@ -0,0 +1,22 @@
+var myObject = {
+
+    x: 1,
+    y: 2,
+    z: 'lalala',
+
+    a: function() {
+        'use strict';
+        return 1;
+    },
+    b: function() {
+        'use strict';
+        return 2;
+    },
+    c: function() {
+        'use strict';
+        return 'lalala';
+    }
+
+};
+
+module.exports = myObject;
