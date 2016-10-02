409a9d7268689f6e440257d629f3889e3d0facee
Hyejin Jun <hyejin.jun@bellevuecollge.edu>
2016-10-02 04:32:55 -0700

    added grunt check

--- Week02-JavaScriptObjects/work.js
+++ Week02-JavaScriptObjects/work.js
@@ -1,8 +1,9 @@
 var person = {
-    firstName: "Hyejin",
-    lastName: "Jun",
+    firstName: 'Hyejin',
+    lastName: 'Jun',
     fullName: function() {
-        return this.firstName + " " + this.lastName
+        'use strict';
+        return this.firstName + ' ' + this.lastName;
     }
 };
 
@@ -15,26 +16,30 @@ var calculator = {
     operator01: person.firstName.length,
     operator02: person.lastName.length,
     add: function() {
-        return this.operator01 + this.operator02
+        'use strict';
+        return this.operator01 + this.operator02;
     },
     subtract: function() {
-        return this.operator01 - this.operator02
+        'use strict';
+        return this.operator01 - this.operator02;
     }
 };
 
 calculator.multiply = function() {
+    'use strict';
     return calculator.operator01 * calculator.operator02;
 };
 
 function divider(title) {
-    console.log("=========================");
+    'use strict';
+    console.log('=========================');
     console.log(title);
-    console.log("=========================");
+    console.log('=========================');
 }
 
 divider('Calculator');
-console.log("operator01: " + calculator.operator01);
-console.log("operator02: " + calculator.operator02);
-console.log("Add: " + calculator.add());
-console.log("Subtract: " + calculator.subtract());
-console.log("Multiply: " + calculator.multiply());
+console.log('operator01: ' + calculator.operator01);
+console.log('operator02: ' + calculator.operator02);
+console.log('Add: ' + calculator.add());
+console.log('Subtract: ' + calculator.subtract());
+console.log('Multiply: ' + calculator.multiply());
