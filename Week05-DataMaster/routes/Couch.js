acc395660a74810d3d4e21edca0b8e35fa01c0d2
Hyejin Jun <hyejin.jun@bellevuecollege.edu>
2016-11-27 19:00:38 -0800

    updated

--- Week05-DataMaster/routes/Couch.js
+++ Week05-DataMaster/routes/Couch.js
@@ -8,7 +8,7 @@ var router = express.Router();
 var fs = require('fs');
 
 var servers = [
-    'http://168.156.47.119:5984',
+    'http://168.156.47.142:5984',
     'http://127.0.0.1:5984',
     'http://192.168.2.19:5984'
 ];
